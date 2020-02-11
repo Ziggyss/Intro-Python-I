9b7bd8919618547ed6951f9dfe3553eb36b85457
Lisa Wilton <mishmao14@gmail.com>
2020-02-11 14:34:02 +0000

    10-13 done

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -12,14 +12,14 @@ for more info regarding list comprehensions.
 
 y = [x for x in range(1, 6)]
 
-print (y)
+print (y, "first one")
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
-for x in range(10):
-    y.append(x**3)
+y = [x**3 for x in range(10)]
+# for x in range(10):
+#     y.append(x**3)
 
 print(y)
 
@@ -28,9 +28,9 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
-for x in a:
-    y.append(x.upper())
+y = [x.upper() for x in a]
+# for x in a:
+#     y.append(x.upper())
 
 print(y)
 
@@ -40,9 +40,9 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
-for i in x:
-    if int(i) % 2 == 0:
-        y.append(int(i))
+y = [i for i in x if int(i) % 2 == 0]
+# for i in x:
+#     if int(i) % 2 == 0:
+#         y.append(int(i))
 
 print(y)
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+F = open("foo.txt", "r")
+print(F.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +19,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+file = open("bar.txt", "w")
+file.write("Arbitrary text \n" )
+file.write("More arbitrary text \n")
+file.write("..and again.... \n")
+
+file.close()
